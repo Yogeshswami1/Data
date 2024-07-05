57473ff7256fa7216eca6266d0490e20f1e32f92
Yogesh <yogeshkumar86077@gmail.com>
2024-07-05 22:02:22 +0530

    third commit

--- data/server/server.js
+++ data/server/server.js
@@ -28,7 +28,7 @@ app.use('/api', enrollRoutes);
 app.use('/api', enrollmentRoutes);
 
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
